9f866c69d68bd559b653ec44a5d1693cfa027aa3
mtbarton <67333876+mtbarton@users.noreply.github.com>
2021-04-24 21:29:10 -0500

    Add files via upload

---
+++ AllMediaDeskTest3/src/AllMediaDeskTest3.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : AllMediaDeskTest3.cpp
+// Author      : Mitchel Barton
+// Version     :
+// Copyright   : Your copyright notice
+// Description : AllMediaDesk Application Task 3
+//============================================================================
+
+#include <iostream>
+#include "matplotlibcpp.h"
+using namespace std;
+
+int main() {
+	plt::plot({1,3,2,4});
+	plt::show();
+}
